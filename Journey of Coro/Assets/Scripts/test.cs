2f0b0ad51b305f509ce34c4f5bd0540d733b57f0
TapZe <nabilmuyassar@Gmail.com>
2022-10-31 15:20:15 +0800

    test

---
+++ Journey of Coro/Assets/Scripts/test.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+{
+    
+}

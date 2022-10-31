406e40d822da11256e0f95e71898216653e93013
MuhamadSahrul <sahrulsmp4@gmail.com>
2022-10-31 14:04:15 +0700

    loading screen

---
+++ Journey of Coro/Assets/Scripts/scene_loader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class scene_loader : MonoBehaviour
+{
+     public Image loadingfill;
+
+    void Start()
+    {
+        StartCoroutine(Loading());
+    }
+
+    IEnumerator Loading()
+    {
+        AsyncOperation loading = SceneManager.LoadSceneAsync("Scene 2");
+
+        while (!loading.isDone)
+        {
+            loadingfill.fillAmount = loading.progress/0.9f;
+            print(loading.progress);
+            yield return null;
+        }
+    } 
+}

c6ef2435105a8f92a02de062a46ef12cddef0492
TapZe <nabilmuyassar@Gmail.com>
2022-12-14 10:22:46 +0800

    Fix Some Bug

--- Journey of Coro/Assets/Scripts/WinLoseHandler.cs
+++ Journey of Coro/Assets/Scripts/WinLoseHandler.cs
@@ -14,6 +14,7 @@ public class WinLoseHandler : MonoBehaviour
     void Start()
     {
         TrackItem.ItemScore = 0;
+        TrackEnemyAwareness.Awareness = 0;
         TrackTimeLimit.TimeLeft = timeLimitInMinutes * 60;
         TrackTimeLimit.TimeMax = timeLimitInMinutes * 60;
     }

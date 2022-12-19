2b6297b4f6e445316e4a2df9e926944322908964
MuhamadSahrul <sahrulsmp4@gmail.com>
2022-12-19 19:46:46 +0700

    volume setting

---
+++ Journey of Coro/Assets/Scripts/Volume.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volume : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    private float musicVolume = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource.volume = musicVolume;
+    }
+
+    public void updateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+}
